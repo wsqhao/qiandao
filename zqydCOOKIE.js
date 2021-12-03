5828b7b751c9866c1046aa9a5b86a3381c173264
wsqhao <35697616+wsqhao@users.noreply.github.com>
2021-12-03 21:40:35 +0800

    Add files via upload

---
+++ zqydCOOKIE.js
@@ -0,0 +1,80 @@
+module.exports = {
+    "id": "zqyd",
+    "name": "钟情阅读",
+    "keys": ["zqydbody","zqydbody2",],
+    "author": "@tom",
+    "settings": [{
+      "id": "zqydSuffix",
+      "name": "当前账号",
+      "val": "1",
+      "type": "number",
+      "desc": "当前抓取ck记录的账号序号，如：1、2、3、4"
+    }, {
+      "id": "zqydCount",
+      "name": "账号个数",
+      "val": "1",
+      "type": "number",
+      "desc": "指定任务最多跑几个账号，根据抓取的账号数据个数来设值"
+    }, {
+      "id": "zqydXH",
+      "name": "循环获取ck",
+      "val": "0",
+      "type": "number",
+      "desc": "0关闭，1打开，默认关闭"
+    }, {
+      "id": "zqydTXTX",
+      "name": "txtx",
+      "val": "0",
+      "type": "number",
+      "desc": "0关闭，1打开，默认关闭"
+    }, {
+      "id": "zqydSC",
+      "name": "sc",
+      "val": "0",
+      "type": "number",
+      "desc": "0关闭，1打开，默认关闭"
+    }, {
+      "id": "zqydnotifyttt",
+      "name": "推送控制",
+      "val": "1",
+      "type": "number",
+      "desc": "0关闭，1推送,默认12点以及23点推送"
+    }, {
+      "id": "zqydnotifyInterval",
+      "name": "通知控制",
+      "val": "2",
+      "type": "number",
+      "desc": "0关闭，1为 所有通知，2为 12，23 点通知，3为 6，12，18，23 点通知"
+    }, {
+      "id": "zqydMinutes",
+      "name": "推送-通知 分钟控制",
+      "val": "10",
+      "type": "number",
+      "desc": "推送以及通知控制在什么分钟段，可设置0-59,默认0到10"
+    }],
+    "repo": "https://gitee.com/tom210120/tom/raw/master/ZQ/zq.js",
+    "icons": ["https://gitee.com/tom210120/tom/raw/master/Tom/Tom.jpg", "https://gitee.com/tom210120/tom/raw/master/Tom/Tom.jpg"],
+    "script": "https://gitee.com/tom210120/tom/raw/master/ZQ/zq.js",
+    "icon": "https://gitee.com/tom210120/tom/raw/master/Tom/Tom.jpg",
+    "favIcon": "mdi-star-outline",
+    "favIconColor": "grey",
+    "datas": [{
+      "key": "zqydbody",
+      "val": "p=qXPkPrW1Kghg%3DLP1JiJujx1M1wd0CoVA0aPrt4J4nA_VL7oyDSztgGhD613ih4LuGjt5_VRTx2kdkG0j2J0olIsl9Fe0SRu8oh_a7YMXCt9h-FGC41WqLH3euoTvNCF7QvfJNXUDUIj5CwUJwKpWuGb0aVUw909q3taauN0mmrIIAtPpPUexEotK54qcsasvVcm434hqKSVEe0y9QN1POEeIXRG4HmGFdju7T5Rl9RsqZefA7qAmK7LTNVs7_ySbz_OCdbUbkToh_sstH9MoUzgviaSLWFq7srBohKF2JEr9XUY39vCgOtnyL02PSZuf2IX9ZRp9ZOwoLFVBuQWF-hlK14d2fhTBKVS7Ta_NFyyDqLtx-JOw56n6lHb_AlxHiCiygfXE-QXH4LM2DLIQUfiuMcCO-I-h4aHL2wUkBcXSbofU54fAMS2bdrNKJWfwiIjlsYJOFBpipiLPiRq8X2iXHwz5fW8IJFMmCC6pQKV5HULDmUFO70y9RDLFI4h6MR8befdpJrxRTThe5vfJQLz3ZyllLi4WoFBxFW48Gt7v6dSimhwCwrWyrdFl_9GXLgIRC1TOl6si0LZAXPJeQnBB14Q7MQjBWRRac-k7U-H9tfair6GI_ftOyvGuXFYpjx7op_eOze8mH0f4BJk3cqmk_N5hjNEf77vM-CHMikxHEyH31yJvDefqzEeASxRDYbbKTXPpIs5_NdsdfkAoxyi3XtDxlOl5xrsFBLycX7QGJOnU8zMfnFLK2-QERKeXYufy1qWtDG-FTmvgkESGz-7dIDVcfeifoNmd820JH0CWJhpUmGnsXdZgq5Gdo9iAb7HeWrMjHrIFOYAG5AbJ8MQmjpRNJyeX7qdcHyFogIBdJceXSkL3-92ElEe2F5V0ipn-NBxgAgmlVm_iKCCKhw9okguQ_21exJbJgOWAcEOR3Uqu1yX4WVvJ8rtZnvhJ9YY-Db2gbJDU9jiotgBusZgs1D2Uyaev134gcc6U3aw7xE01lK6jTRMr_SLic5SBroOo0KsyBEfuUBd_uusy6nUVdyzuj95MFCPMTsObpQs94Czc-ElsrjY19vT0zPcQPxzkCdI4d_gKS98ANbe1jtVyVDf9Ao7bWseCX7wUvRhr7uMi_ciqX1zfmzFXlBYrKrK0c4ztIC9qSp24F20aneTFzZARb8X8JUjqeHJhAf0RR_vRBFdEG9gYi2hNJqNpunVEdArYPAuBcNcPGRkNOs9fMjja2sfD7emuvL5Pg33RP"
+    }, {
+      "key": "zqydbody2",
+      "val": ""
+    }, {
+      "key": "zqydbody3",
+      "val": ""
+    }, {
+      "key": "zqydbody4",
+      "val": ""
+    }, {
+      "key": "zqydbody5",
+      "val": ""
+    }],
+    "sessions": [],
+    "isLoaded": true
+  }
+  
